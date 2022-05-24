c2ca64e2d55998011514a385b699f426214b5cf1
scarfacedotcom <peteradaaku@gmail.com>
2022-05-25 00:21:53 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import "./App.css";
+import React from 'react';
+import Loading from './Loading';
 
 function App() {
+  const [tasks, setTasks] = useState([]);
+  const [loading, setLoading] = useState(true);
+  // const [error, setError] = useState("");
+
+  useEffect(() => {
+    fetch("http://localhost:3000/tasks")
+      .then((res) => {
+        return res.json();
+      })
+      .then((data) => {
+        setLoading(false);
+        setTasks(data);
+      })
+      .catch((error) => console.log(error));
+  }, []);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1 align="center">TASK MANAGER</h1>
+      {loading ? (
+        <Loading />
+      ) : (
+        <ul>
+          {tasks.map((task) => {
+            return <li>{task.taskName}</li>;
+          })}
+        </ul>
+      )}
     </div>
   );
 }
---
+++ src/Loading.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+
+function Loading (props) {
+  return (
+    <div class="lds-spinner"><div></div><div></div><div></div></div>
+  );
+}
+ 
+export default Loading;
